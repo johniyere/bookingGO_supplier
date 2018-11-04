2ed38b119bf9754ec42bc830d83dd8ee2ea97eb5
John Iyere <john.iyere@student.manchester.ac.uk>
2018-11-04 14:38:08 +0000

    Basic user input linked and data retrival

--- app.js
+++ app.js
@@ -1,24 +1,55 @@
 const https = require('https');
+const prompt = require('prompt');
+ 
+//
+// Start the prompt
+//
+prompt.start();
 
-const apiString = "https://techtest.rideways.com/dave?pickup=3.410632,-2.157533&dropoff=3.410632,-2.157533"
-
-https.get(apiString, (resp) => {
-  let data = '';
-
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    let result = JSON.parse(data);
-
-    result.options.forEach(option => {
-      let output = `${option.car_type} - ${option.price}`;
-      console.log(output);
-    });
-  });
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
+//
+// Get two properties from the user: username and email
+//
+prompt.get(['latitude1', 'longitude1', 'latitude2', 'longitude2'], function (err, result) {
+  //
+  // Log the results.
+  //
+  console.log('Pickup Location');
+  console.log('  latitude: ' + result.latitude1);
+  console.log('  longitude: ' + result.longitude1);
+  console.log('Dropoff Location');
+  console.log('  latitude: ' + result.latitude2);
+  console.log('  longitude: ' + result.longitude2);
+  
+  const apiString = getApiString(result)
+  console.log(getApiString(result))
+  retrieveData(apiString)
 });
+
+function getApiString(result) {
+  const pickupLocation = `${result.latitude1},${result.longitude1}`;
+  const dropOffLocation = `${result.latitude2},${result.longitude2}`;
+  return `https://techtest.rideways.com/dave?pickup=${pickupLocation}&dropoff=${dropOffLocation}`
+}
+
+function retrieveData(apiString) {
+  https.get(apiString, (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      let result = JSON.parse(data);
+      console.log(result)
+      result.options.forEach(option => {
+        let output = `${option.car_type} - ${option.price}`;
+        console.log(output);
+      });
+    });
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+}
